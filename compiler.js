b349e9b1d89a80f8723b0e71eb2c1fe13e2657b7
Dmitry Olshansky <dmitry@olshansky.me>
2020-01-17 01:45:57 +0300

    Move on with compiler

--- compiler.js
+++ compiler.js
@@ -6,10 +6,13 @@ const repeat = peg.repeat
 const any = peg.any
 const seq = peg.seq
 
+// building blocks
 const ws = peg.skipWhile(c => c == ' ' || c == '\t' || c == '\v' || c == '\r' || c == '\n')
-
+const opt = parser => repeat(parser, 0, 1).map(v => v.length ? v[0] : null)
+const listOf = parser => seq(parser, repeat(seq(lit(','), parser)))/*.map(pair => [pair[0], ...pair[1].map(x => x[1])])*/
 const lit = text => peg.match(text).skipping(ws)
 
+// building up lexical for types
 const comment = seq(peg.match('#'), peg.skipWhile(c => c != '\n' && c != '\r')).skipping(ws)
 
 const id = peg.matchWhile(c => /\w[\w\d]*/.test(c), 1).skipping(ws)
@@ -28,18 +31,31 @@ const generic_type_arg = any(builtin_type, number)
 
 const generic_type = seq(id, lit('['), generic_type_arg, repeat(seq(lit(','), generic_type_arg)), lit(']'))
 
-const basic_type_expr = any(builtin_type, generic_type, struct_def)
+const basic_type_expr = any(builtin_type, generic_type, struct_def, id)
 
-peg.bind(type_expr_ref, any(
+peg.bind(type_expr, any(
     basic_type_expr, repeat(seq(lit('&'), basic_type_expr))
 ))
 
-const type_def = seq([type, id, lit('='), type_expr])
+// this is type definition
+const type_def = seq(lit('type'), id, lit('='), type_expr)
 
-exports.input = function (input) {
-    return protocol_input(input)
-}
+const ids = listOf(id)
+const arg = seq(id, lit(':'), type_expr)
+const args = listOf(arg)
+const method = seq(lit('def'), id, lit('('), opt(args), lit(')'), opt(seq(lit(':'), type_expr)))
+const methods = repeat(method)
+// this is protocol definition
+const proto_def = seq(lit('proto'), id, opt(seq(lit(':'), ids)), lit('{'), methods, lit('}'))
+
+
 exports.number = number
 exports.id = id
 exports.generic_type = generic_type
+exports.type_expr = type_expr
+// top-level stuff
+exports.type_def = type_def
+exports.comment = comment
+exports.proto_def = proto_def
+
 
--- compiler_test.js
+++ compiler_test.js
@@ -1,7 +1,11 @@
+const test = require('ava')
 const compiler = require('./compiler')
 
-const inp = compiler.input
-
-console.log(compiler.number(inp('20')))
-console.log(compiler.id(inp('u8')))
-console.log(compiler.generic_type(inp('Array[u8, 20]')))
+test('parse types', t => {
+    t.deepEqual(compiler.number.parse('20'), '20')
+    t.deepEqual(compiler.id.parse('u8'), 'u8')
+    t.deepEqual(compiler.generic_type.parse('Array[u8, 20]'), ['Array', '[', 'u8', [[',', '20']], ']'])
+    //t.deepEqual(compiler.type_expr.parse('struct {}'), undefined)
+    //t.deepEqual(compiler.type_def.parse('type A = struct {a:C&D b:u8}'), undefined)
+    t.deepEqual(compiler.proto_def.parse('proto A : E, F { def fn(b: B, c: C):D }'), undefined)
+})
